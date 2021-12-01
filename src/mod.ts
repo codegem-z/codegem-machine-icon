b225755529ed0861d285bfa905fd44cc0554042b
ben <x_z_gfree@163.com>
2021-12-01 21:36:03 +0800

    fix: fix __dirname not fund in es module.

--- src/mod.ts
+++ src/mod.ts
@@ -3,7 +3,12 @@ import path from 'path';
 import fs from 'fs';
 import handlebars from 'handlebars';
 import _ from 'lodash';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import { importDir } from './utils.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const template = fs.readFileSync(
   path.join(__dirname, './template/taro.handlebars'),
